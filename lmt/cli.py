9c7b1b701617dce390ae3ee437fcf9cf36ee9c37
sderev <contact@sderev.com>
2023-06-23 20:54:11 +0200

    Refactor: exposed the functionalities into a lib

--- lmt/cli.py
+++ lmt/cli.py
@@ -1,18 +1,10 @@
 import filecmp
 import shutil
-import subprocess
 import sys
-from pathlib import Path
 
 import click
-import openai
-import yaml
-from click.exceptions import BadOptionUsage
-from rich.console import Console
-from rich.live import Live
-from rich.markdown import Markdown
 
-from lmt import gpt_integration as openai_utils
+from .lib import *
 
 VALID_MODELS = {
     "chatgpt": "gpt-3.5-turbo",
@@ -25,15 +17,10 @@ VALID_MODELS = {
     "gpt4-32k": "gpt-4-32k",
 }
 
-EMOJI_MESSAGE = (
-    ". Add plenty of emojis as a colorful way to convey emotions. However, don't"
-    " mention it."
-)
 
-
-def validate_model(ctx, param, value):
+def validate_model_name(ctx, param, value):
     """
-    Validates the model parameter.
+    Validates the model name parameter.
     """
     model_name = value.lower()
     if model_name in VALID_MODELS:
@@ -67,7 +54,7 @@ def lmt():
 
 @lmt.command()
 @click.argument(
-    "prompt",
+    "prompt_input",
     type=str,
     required=False,
     nargs=-1,
@@ -77,7 +64,7 @@ def lmt():
     "-m",
     default="gpt-3.5-turbo",
     help="The model to use for the requests.",
-    callback=validate_model,
+    callback=validate_model_name,
 )
 @click.option(
     "--template",
@@ -134,7 +121,7 @@ def prompt(
     tokens,
     no_stream,
     raw,
-    prompt,
+    prompt_input,
     debug,
 ):
     """
@@ -142,30 +129,28 @@ def prompt(
 
     Example: lmt prompt "Say hello" --emoji
     """
-    if not prompt:
+    if not prompt_input:
         if template:
             pass
         elif not sys.stdin.isatty():
-            prompt = sys.stdin.read()
+            prompt_input = sys.stdin.read()
         elif sys.stdin.isatty():
             click.echo(
                 click.style(
                     (
-                        "You can paste your prompt below.\nOnce you've done, press"
-                        " Ctrl+D (or Ctrl+Z on Windows) to send it."
+                        "You can paste your prompt below. Press <Enter> to"
+                        " validate.\nOnce you've done, press Ctrl+D (or Ctrl+Z on"
+                        " Windows) to send it."
                     ),
                     fg="yellow",
                 )
                 + "\n---"
             )
-            prompt = sys.stdin.read()
+            prompt_input = sys.stdin.read()
             click.echo()
-    prompt = "".join(prompt)
+    prompt_input = "".join(prompt_input).rstrip()
 
-    if not system:
-        system = ""
-
-    if template and system:
+    if system and template:
         raise click.BadOptionUsage(
             option_name="template",
             message=click.style(
@@ -174,86 +159,24 @@ def prompt(
             ),
         )
 
-    if template:
-        template_content = get_template(template)
-        system = update_from_template(template_content, "system", system)
-        prompt = update_from_template(template_content, "user", prompt)
-        model = template_content.get("model", model) or model
-
-    if emoji:
-        system += EMOJI_MESSAGE
-
-    prompt = openai_utils.format_prompt(system, prompt)
-
-    if debug:
-        display_debug_information(prompt, model, temperature)
-
-    if tokens:
-        full_prompt = prompt[0]["content"] + prompt[1]["content"]
-        number_of_tokens = openai_utils.num_tokens_from_string(full_prompt, model)
-        cost = openai_utils.estimate_prompt_cost(prompt)[model]
-
-        click.echo(
-            "Number of tokens in the prompt:"
-            f" {click.style(str(number_of_tokens), fg='yellow')}."
-        )
-        click.echo(
-            f"Cost of the prompt for the {click.style(model, fg='blue')} model is:"
-            f" {click.style(f'${cost}', fg='yellow')}."
-        )
-        click.echo(
-            "Please note that this cost applies only to the prompt, not the"
-            " subsequent response."
-        )
-        sys.exit(0)
-
-    stream = False if no_stream else True
-
-    markdown_stream = ""
-    with Live(Markdown(markdown_stream), refresh_per_second=25) as live:
-
-        def update_markdown_stream(chunk: str) -> None:
-            nonlocal markdown_stream
-            markdown_stream += chunk
-            if raw:
-                print("".join(chunk), end="")
-            else:
-                rich_markdown_stream = Markdown(markdown_stream)
-                live.update(rich_markdown_stream)
-
-        try:
-            content, response_time, response = openai_utils.chatgpt_request(
-                prompt,
-                model=model,
-                stream=stream,
-                temperature=temperature,
-                update_markdown_stream=update_markdown_stream,
-            )
-            if not stream:
-                print(content)
-
-        except openai.error.RateLimitError as error:
-            click.echo(click.style("Error: ", fg="red") + f"{error}")
-            handle_rate_limit_error()
-        except Exception as error:
-            click.echo(click.style(f"Error: {error}", fg="red"))
-
-
-def update_from_template(template_content, key, value):
-    """
-    Updates the value of a key from a template.
-    """
-    existing_value = template_content.setdefault(key, "")
-    if existing_value is None:
-        template_content[key] = existing_value = ""
-
-    return template_content.get(key, value) + (value or "")
+    prepare_and_generate_response(
+        system,
+        template,
+        model,
+        emoji,
+        prompt_input,
+        temperature,
+        tokens,
+        no_stream,
+        raw,
+        debug,
+    )
 
 
 @lmt.group()
 def templates():
     """
-    Manage templates.
+    Manage the templates.
     """
     pass
 
@@ -325,9 +248,8 @@ def add_template(template):
             return
 
     template_file = TEMPLATES_DIR / f"{template}.yaml"
-    default_template_file = get_default_template_file()
+    default_template_file = get_default_template_file_path()
 
-    # Copy the default template to the new file
     shutil.copyfile(default_template_file, template_file)
 
     click.edit(filename=template_file)
@@ -391,167 +313,3 @@ def rename_template(template):
             + f"The template '{template}' does not exist."
         )
 
-
-def get_template(template):
-    """
-    Reads the template YAML file and returns a dictionary with its content.
-    """
-    template_file = TEMPLATES_DIR / f"{template}.yaml"
-
-    try:
-        with open(template_file, "rt") as file:
-            template_content = yaml.safe_load(file)
-    except FileNotFoundError:
-        click.echo(
-            click.style("Error: ", fg="red")
-            + f"The template '{click.style(template, fg='red')}' does not exist."
-        )
-        sys.exit(1)
-    else:
-        return template_content
-
-
-def get_templates_dir():
-    """
-    Returns the path to the templates directory.
-    """
-    templates_dir = Path.home() / ".config" / "lmt" / "templates"
-    templates_dir.mkdir(parents=True, exist_ok=True)
-    return templates_dir
-
-
-def get_default_template_file():
-    """
-    Returns the path to the default template.
-    """
-    default_dir = Path.home() / ".config" / "lmt" / "default"
-    default_dir.mkdir(parents=True, exist_ok=True)
-
-    default_template_file = default_dir / "template.yaml"
-    if not default_template_file.exists():
-        click.echo("The default template does not exist. Creating it...")
-        with open(default_template_file, "w") as file:
-            file.write(DEFAULT_TEMPLATE_CONTENT)
-    return default_template_file
-
-
-def display_debug_information(prompt, model, temperature):
-    """
-    Displays debug information.
-    """
-    click.echo()
-    click.echo("---\n" + click.style("Debug information:", fg="yellow"))
-    click.echo()
-
-    click.echo(click.style("Prompt:", fg="red"), nl=False)
-    for role in prompt:
-        click.echo()
-        click.echo(click.style(f"{role['role']}:", fg="blue"))
-        click.echo(f"{role}")
-    click.echo()
-
-    click.echo(click.style("Model:", fg="red"))
-    click.echo(f"{model=}")
-    click.echo()
-
-    click.echo(click.style("Temperature:", fg="red"))
-    click.echo(f"{temperature=}")
-    click.echo()
-
-    click.echo(click.style("End of debug information.", fg="yellow"))
-    click.echo("---\n")
-
-
-def handle_rate_limit_error():
-    """
-    Provides guidance on how to handle a rate limit error.
-    """
-    click.echo()
-    click.echo(
-        click.style(
-            (
-                "You might not have set a usage rate limit in your"
-                " OpenAI account settings. "
-            ),
-            fg="blue",
-        )
-    )
-    click.echo(
-        "If that's the case, you can set it"
-        " here:\nhttps://platform.openai.com/account/billing/limits"
-    )
-
-    click.echo()
-    click.echo(
-        click.style(
-            "If you have set a usage rate limit, please try the following steps:",
-            fg="blue",
-        )
-    )
-    click.echo("- Wait a few seconds before trying again.")
-    click.echo()
-    click.echo(
-        "- Reduce your request rate or batch tokens. You can read the"
-        " OpenAI rate limits"
-        " here:\nhttps://platform.openai.com/account/rate-limits"
-    )
-    click.echo()
-    click.echo(
-        "- If you are using the free plan, you can upgrade to the paid"
-        " plan"
-        " here:\nhttps://platform.openai.com/account/billing/overview"
-    )
-    click.echo()
-    click.echo(
-        "- If you are using the paid plan, you can increase your usage"
-        " rate limit"
-        " here:\nhttps://platform.openai.com/account/billing/limits"
-    )
-    click.echo()
-
-
-TEMPLATES_DIR = get_templates_dir()
-
-DEFAULT_TEMPLATE_CONTENT = """# Documentation: https://github.com/sderev/lmt
-
-# You can leave either of the fields empty. 
-
-
-# Here, you can instruct how you want ChatGPT to behave.
-# For example, you might say:
-# "You are an AI modeled after Emil Cioran, the Romanian philosopher and essayist.
-# You have a deep understanding of existentialism and philosophical pessimism."
-# The more precise and detailed, the better!
-system:
-
-
-
-
-# Here, you should write what you want to say to ChatGPT.
-# For example: "As a language model yourself, how do you view your own existence?"
-user:
-
-
-
-
-# You can change the model according to the list below.
-# The default model is "gpt-3.5-turbo".
-# More advanced models might provide more accurate or detailed responses,
-# but they also have a higher "cost per 1K tokens". Tokens refer to chunks of text that AI models read. More tokens usually mean more cost.
-model: "gpt-3.5-turbo"
-
-
-# Valid Models
-#
-# "chatgpt" or "gpt-3.5-turbo"
-# "chatgpt-16k" or "gpt-3.5-turbo-16k"
-#
-# "3.5" or "gpt-3.5-turbo"
-# "3.5-16k" or "gpt-3.5-turbo-16k"
-#
-# "4" or "gpt-4"
-# "gpt4" or "gpt-4"
-#
-# "4-32k" or "gpt-4-32k"
-# "gpt4-32k" or "gpt-4-32k"
-"""
